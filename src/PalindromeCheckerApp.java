b49231a36e4db3fe071693ee584d0c23aa3b1631
js3382@srmist.edu.in <your GitHub email>
2026-03-18 09:12:39 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "radar";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
